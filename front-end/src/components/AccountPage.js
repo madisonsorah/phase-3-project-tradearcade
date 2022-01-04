ebb46762c47d717d1ca063db5340a3c3341a7e1f
madison sorah <madisonsorah@gmail.com>
2022-01-04 16:36:02 -0500

    Updated styling on AccountPage

--- front-end/src/components/AccountPage.js
+++ front-end/src/components/AccountPage.js
@@ -10,18 +10,19 @@ function AccountPage() {
                 <h1 className="accountPageH1">My Account</h1>
                 <div className="accountPageListLeft">
                     <ul className="accountPageUl">
-                        <Link to="/tradegame" className="accountPageLink">Trade in a game</Link>
-                        <Link to="/tradehistory" className="accountPageLink">Game trade history</Link>
-                        <Link to="/gamereviews" className="accountPageLink">Game reviews</Link>
+                        <Link to="/tradegame" className="accountPageLink">Trade in Game</Link>
+                        <Link to="/tradehistory" className="accountPageLink">Game Trade History</Link>
+                        <Link to="/gamereviews" className="accountPageLink">Game Reviews</Link>
                         <Link to="/gamewishlist" className="accountPageLink">Game Wishlist</Link>
                     </ul>
                 </div>
                 <div className="accountPageListRight">
                     <ul className="accountPageUl">
                         <Link to="/followers" className="accountPageLink">Followers</Link>
-                        <Link to="/followlist" className="accountPageLink">Members you follow</Link>
+                        <Link to="/followlist" className="accountPageLink">Members You Follow</Link>
                     </ul>
                 </div>
+                <p className="accountPageLogout">Logout</p>
             </div>
         </div>
     )
