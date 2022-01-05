69880178f24f59cd595b26342d0106c19682897a
madison sorah <madisonsorah@gmail.com>
2022-01-05 12:25:51 -0500

    Formatted address form in SendGame component

--- front-end/src/components/SendGame.js
+++ front-end/src/components/SendGame.js
@@ -1,34 +1,28 @@
 import React from "react";
 import NavBar from "./NavBar";
 
-function GamesSent() {
+function SendGame() {
     return (
         <div>
             <NavBar />
-            <div>
-                <form>Enter Your Address
-                    <input placeholder="First Name"></input>
-                    <input placeholder="Last Name"></input>
-                    <input placeholder="Address Line 1"></input>
-                    <input placeholder="Address Line 2 (optional)"></input>
-                    <input placeholder="City"></input>
-                    <input placeholder="State"></input>
-                    <input placeholder="Zip Code"></input>
-                    <p>Your shipping label is free!</p>
-                    <button>GET MY LABEL</button>
-                </form>
-                <p>A shipping label has been sent to your email. Print out and use on any box.</p>
-            </div>
-            <div>
-                <h1>Games Sent</h1>
-                <ul>
-                    <li>Final Fantasy X - PlayStation 4</li>
-                    <li>Soul Calibur - GameCube</li>
-                    <li>The Legend of Zelda: Windwaker - Wii</li>
-                </ul>
+            <div className="sendGameDiv">
+                <div className="sendGameFormDiv">
+                    <form><p className="sendGameFormEnterAddress">Enter Your Address</p>
+                        <input className="sendGameFormInput" placeholder="First Name"></input>
+                        <input className="sendGameFormInput" placeholder="Last Name"></input>
+                        <input className="sendGameFormInput" placeholder="Address Line 1"></input>
+                        <input className="sendGameFormInput" placeholder="Address Line 2 (optional)"></input>
+                        <input className="sendGameFormInput" placeholder="City"></input>
+                        <input className="sendGameFormInput" placeholder="State"></input>
+                        <input className="sendGameFormInput" placeholder="Zip Code"></input>
+                        <p className="sendGameFormFreeLabel">Your shipping label is free!</p>
+                        <button className="sendGameFormButton">GET MY LABEL</button>
+                    </form>
+                    <p>A shipping label has been sent to your email. Print out and use on any box.</p>
+                </div>
             </div>
         </div>
     )
 }
 
-export default GamesSent;
+export default SendGame;
--- front-end/src/components/TradeHistory.js
+++ front-end/src/components/TradeHistory.js
@@ -1,12 +1,22 @@
 import React from "react";
 import NavBar from "./NavBar"
 
-function GamesBorrowed() {
+function TradeHistory() {
     return (
         <div>
             <NavBar />
+            <div>
+            <div className="tradeHistorySentListDiv">
+                <h1>Games Sent</h1>
+                <ul className="sentGameListUl">
+                    <li>Final Fantasy X - PlayStation 4</li>
+                    <li>Soul Calibur - GameCube</li>
+                    <li>The Legend of Zelda: Windwaker - Wii</li>
+                </ul>
+            </div>
+            </div>
         </div>
     )
 }
 
-export default GamesBorrowed;
+export default TradeHistory;
