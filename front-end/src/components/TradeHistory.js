827e08bc956fcb6ae2ae7d6cc3559cac62cd9961
madison sorah <madisonsorah@gmail.com>
2022-01-05 12:33:43 -0500

    Started formatting TradeHistory component

--- front-end/src/components/TradeHistory.js
+++ front-end/src/components/TradeHistory.js
@@ -5,15 +5,23 @@ function TradeHistory() {
     return (
         <div>
             <NavBar />
-            <div>
-            <div className="tradeHistorySentListDiv">
-                <h1>Games Sent</h1>
-                <ul className="sentGameListUl">
-                    <li>Final Fantasy X - PlayStation 4</li>
-                    <li>Soul Calibur - GameCube</li>
-                    <li>The Legend of Zelda: Windwaker - Wii</li>
-                </ul>
-            </div>
+            <div className="sendGameDiv">
+                <div className="tradeHistorySentListDiv">
+                    <h1>Games Sent</h1>
+                    <ul className="tradeHistorySentListUl">
+                        <li>Final Fantasy X - PlayStation 4</li>
+                        <li>Soul Calibur - GameCube</li>
+                        <li>The Legend of Zelda: Windwaker - Wii</li>
+                    </ul>
+                </div>
+                <div className="tradeHistoryReceivedListDiv">
+                    <h1>Games Received</h1>
+                    <ul className="tradeHistoryReceivedListUl">
+                        <li>Ni No Kuni: Wrath of the White Witch - PlayStation 4</li>
+                        <li>Hades - Nintendo Switch</li>
+                        <li>GoldenEye 007 - Nintendo 64</li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
