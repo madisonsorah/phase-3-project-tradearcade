476a6c7ba7dae3673d0748b238d19a23d074f572
madison sorah <madisonsorah@gmail.com>
2022-01-05 12:44:41 -0500

    Tweaked formatting in TradeHistory component

--- front-end/src/components/TradeHistory.js
+++ front-end/src/components/TradeHistory.js
@@ -5,21 +5,21 @@ function TradeHistory() {
     return (
         <div>
             <NavBar />
-            <div className="sendGameDiv">
+            <div className="tradeHistoryDiv">
                 <div className="tradeHistorySentListDiv">
-                    <h1>Games Sent</h1>
+                    <h1 className="tradeHistorySentListH1">Games Sent</h1>
                     <ul className="tradeHistorySentListUl">
-                        <li>Final Fantasy X - PlayStation 4</li>
-                        <li>Soul Calibur - GameCube</li>
-                        <li>The Legend of Zelda: Windwaker - Wii</li>
+                        <li className="tradeHistorySentListLi">Final Fantasy X - PlayStation 4</li>
+                        <li className="tradeHistorySentListLi">Soul Calibur - GameCube</li>
+                        <li className="tradeHistorySentListLi">The Legend of Zelda: Windwaker - Wii</li>
                     </ul>
                 </div>
                 <div className="tradeHistoryReceivedListDiv">
-                    <h1>Games Received</h1>
+                    <h1 className="tradeHistoryReceivedListH1">Games Received</h1>
                     <ul className="tradeHistoryReceivedListUl">
-                        <li>Ni No Kuni: Wrath of the White Witch - PlayStation 4</li>
-                        <li>Hades - Nintendo Switch</li>
-                        <li>GoldenEye 007 - Nintendo 64</li>
+                        <li className="tradeHistoryReceivedListLi">Ni No Kuni: Wrath of the White Witch - PlayStation 4</li>
+                        <li className="tradeHistoryReceivedListLi">Hades - Nintendo Switch</li>
+                        <li className="tradeHistoryReceivedListLi">GoldenEye 007 - Nintendo 64</li>
                     </ul>
                 </div>
             </div>
