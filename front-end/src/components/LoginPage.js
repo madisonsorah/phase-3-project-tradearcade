7a39d982250a756893f90408dd96efa1511ba144
Kenneth Kim <kkm2059@gmail.com>
2022-01-05 19:00:29 -0500

    Unique Emails on Account Creation and half working login

--- front-end/src/components/LoginPage.js
+++ front-end/src/components/LoginPage.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NavBar from "./NavBar"
 import { Link } from "react-router-dom";
+import { findDOMNode } from "react-dom";
 
-function LoginPage() {
+function LoginPage(currentUser, setCurrentUser) {
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+    const [allUsers, setAllUsers] = useState()
 
+    useEffect(() => {
+        fetch("http://localhost:9292/users")
+        .then(resp => resp.json())
+        .then(users => handleFetch(users))
+    },[])
+
+    function handleFetch(users) {
+        setAllUsers(users)
+    }
+
+    function handleLogin(e) {
+        e.preventDefault()
+        // console.log(email)
+        // console.log(password)
+        const validEmails = (allUsers.filter((user) => user.email == email))
+        console.log(validEmails)
+        const correctPassword = validEmails.map(a => a.password)
+        console.log(correctPassword)
+        if (password == correctPassword){
+            console.log("Logged In!")
+            // setCurrentUser(validEmails)
+            console.log(currentUser)
+        }else {
+            console.log("Incorrect Credentials!")
+        }
+    }
+    
     return (
         <div>
             <NavBar />
@@ -11,9 +42,9 @@ function LoginPage() {
                 <h1>Welcome back!</h1>
                 <div className="loginPageFormDiv">
                     <form>
-                        <input className="loginPageFormInput" placeholder="Email"></input>
-                        <input className="loginPageFormInput" placeholder="Password"></input>
-                        <button className="loginPageFormButton">Login</button>
+                        <input className="loginPageFormInput" placeholder="Email" onChange={(e) => setEmail(e.target.value)}></input>
+                        <input className="loginPageFormInput" placeholder="Password"onChange={(e) => setPassword(e.target.value)}></input>
+                        <button className="loginPageFormButton" onClick={(e) => handleLogin(e)}>Login</button>
                     </form>
                 </div>
                 <div>
--- front-end/src/components/NavBar.js
+++ front-end/src/components/NavBar.js
@@ -2,13 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 
-function NavBar() {
+function NavBar(currentUser, setCurrentUser) {
     return (
         <div className="mainNavDiv">
             <ul className="navBarUl">
                 <li className="navBarLi">TradeArcade</li>
                 <li className="navBarLi">Browse Games</li>
-                <Link to="/login" className="navBarLink">Login</Link>
+                <Link to="/login" className="navBarLink" currentUser={currentUser} setCurrentUser={setCurrentUser}>Login</Link>
                 <Link to="/account" className="navBarLink">Account</Link>
             </ul>
         </div>
