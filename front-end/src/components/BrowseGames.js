7dd5059380fffdbb92348bc6f82a72e82faf5c88
madison sorah <madisonsorah@gmail.com>
2022-01-06 12:18:57 -0500

    Updates to backend data and fetch

--- front-end/src/components/BrowseGames.js
+++ front-end/src/components/BrowseGames.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "./NavBar"
 
 function BrowseGames() {
-    // renderedGames = gameData.map((game) => {
-    //     return (
-    //         <div>
-    //             <img src={game.img}></img>
-    //             <h2>{game.title}</h2>
-    //             <p>{game.platform}</p>
-    //         </div>
-    //     )
-    // })
+    const [games, setGames] = useEffect([])
+
+    useEffect(() => {
+        fetch('http://localhost:9292/games')
+        .then((response) => response.json())
+        .then((gameData) => setGames(gameData))
+    }, []);
+
+
+    renderedGames = games.map((game) => {
+        return (
+            <div>
+                <img src={game.img}></img>
+                <h2>{game.title}</h2>
+                <p>{game.platform}</p>
+            </div>
+        )
+    })
 
     return (
         <div>
